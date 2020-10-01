bd5fdd2ba130b0cbdbe174559d3a824353313cb6
nasrinsamad19 <nasrinsamad19@gmail.com>
2020-10-01 17:11:32 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+import 'package:sample/Contacts.dart';
+import 'package:sample/EmployeeHome.dart';
+import 'package:sample/Images.dart';
+import 'package:sample/Login.dart';
+import 'package:sample/Home.dart';
+import 'package:sample/SearchList.dart';
+import 'package:sample/ShowImage.dart';
+import 'package:sample/DBTestPage .dart';
+import 'package:sample/studentsTable.dart';
+import 'package:sample/UserHome.dart';
+import 'package:sample/DBHelper.dart';
+import 'package:sample/PlatformCheck.dart';
+
+
+void main() => runApp(MyApp());
+
+ class MyApp extends StatelessWidget{
+  @override
+   Widget build(BuildContext buildContext)
+  {
+    return MaterialApp(
+      home:EmployeeHome()
+    );
+  }}
