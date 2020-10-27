f9ac5c50efdc47e0517f4e59e09f8f3728462231
nasrinsamad19 <nasrinsamad19@gmail.com>
2020-10-27 11:19:41 +0530

    Added MVVM sample

---
+++ lib/mvvm/mvvm_provider.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'mvvm_ex.dart';
+import 'package:email_validator/email_validator.dart';
+class SubscriptionViewModelImpl implements SubscriptionViewModel {
+  var _mailTextController = StreamController<String>.broadcast();
+
+  @override
+  Sink get inputMailText => _mailTextController;
+
+  @override
+  Stream<bool> get outputIsButtonEnabled =>
+      _mailTextController.stream
+          .map((email) => EmailValidator.validate(email));
+
+  @override
+  Stream<String> get outputErrorText =>
+      outputIsButtonEnabled
+          .map((isEnabled) => isEnabled ? null : "Invalid email");
+
+  @override
+  void dispose() => _mailTextController.close();
+
+
+
+}
